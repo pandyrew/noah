485fbc370c7b586cefb2740c3971bca0ce06b0b4
Andrew Hwang <pandy430@gmail.com>
2025-01-21 11:51:16 -0800

    bye

--- app/page.js
+++ app/page.js
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-6xl font-bold text-center">Happy Birthday Noah!</h1>
+    <div className="min-h-screen flex items-center justify-center relative">
+      <Image
+        src="/noah.JPG"
+        alt="Background"
+        fill
+        className="object-cover z-0"
+        priority
+      />
+      <h1 className="text-6xl font-bold text-center text-white z-10 relative">
+        Happy Birthday Noah!
+      </h1>
     </div>
   );
 }
